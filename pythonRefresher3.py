99450878dbbdd47ae900af6119c90153c517c968
uthapa82 <tupendra022@gmail.com>
2022-03-12 09:19:11 -0500

    modules

---
+++ pythonRefresher3.py
@@ -0,0 +1,15 @@
+#A module is basically a file contatining a set of functions to include in your application.
+# There are core python modules, modules you can install using the pip package manager 
+
+import datetime
+from datetime import date 
+import time 
+from time import time 
+
+#today = datatime.date.today()
+today = date.today()
+print(today)
+
+timestamp = time()
+print(timestamp)
+
